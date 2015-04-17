3a291e91e0c0304d72208fea680997223d9629e5
locomalo <elleandro@gmail.com>
2015-04-17 10:18:43 -0300

    .

--- ejemplos/test/layout/load.tcl
+++ ejemplos/test/layout/load.tcl
@@ -1,12 +1,15 @@
 #-----------------------------------------------------
 
 box 0 0 0 0
-
-lef read /usr/local/share/qflow/tech/osu035/osu035_stdcells.lef
+# Carga las celdas estándar:
+lef read ../../../tech/osu035/osu035_stdcells.lef
+#lef read /usr/local/share/qflow/tech/osu035/osu035_stdcells.lef
 
 # def read map9v3_route.def
+# Carga el diseño
 def read map9v3.def
 
 grid 1.6um 2.0um
 
 snap grid
+
---
+++ ejemplos/test/netlist/run_sim.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Primero lanza el simulador de verilog. 
+iverilog -o map9v3_rtl -c input_files.txt -Ttyp
+vvp map9v3_rtl 
+# Lanza el visor de ondas para ver el archivo VCD. Usa un archivo .gtkw donde está guardadas la configuración del visor para estas señales.
+gtkwave map9v3_tb.vcd map9v3_tb.gtkw
